6c8dc727269570a31e80fdc464800e72f741d410
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-05-30 22:06:33 +0800

    Add files via upload
    Refactored all the code, arranged the files
    Made the "read more" functionality in JS
    Beautified the website and added white spaces

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let seeMore = document.querySelector
+const readMoreBtn = document.querySelector('.read-more-btn')
+const text = document.querySelector('.text')
+
+readMoreBtn.addEventListener('click', (e)=>{
+    text.classList.toggle('show-more');
+    if(readMoreBtn.innerText === 'read more'){
+        readMoreBtn.innerText = 'read less'
+    }else{
+        readMoreBtn.innerText = 'read more'
+    }
+
+})
