1eb5d231681a8dc7ae3021ed047af1d83c17eacc
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-06-01 18:54:57 +0800

    refactor

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const readMoreBtn = document.querySelector('.read-more-btn')
 const text = document.querySelector('.text')
-
+const readpop = document.querySelector('.readpop')
+const speakpop = document.querySelector('.speakpop')
+const listenpop = document.querySelector('.listenpop')
+const writepop = document.querySelector('.writepop')
 readMoreBtn.addEventListener('click', (e)=>{
     text.classList.toggle('show-more');
     if(readMoreBtn.innerText === 'read more'){
@@ -9,4 +12,20 @@ readMoreBtn.addEventListener('click', (e)=>{
         readMoreBtn.innerText = 'read more'
     }
 
-})
+});
+
+readpop.addEventListener('click', ()=>{
+    readpop.classList.toggle('pop');
+} )
+
+speakpop.addEventListener('click', ()=>{
+   speakpop.classList.toggle('pop');
+} )
+
+listenpop.addEventListener('click', ()=>{
+    listenpop.classList.toggle('pop');
+} )
+
+writepop.addEventListener('click', ()=>{
+    writepop.classList.toggle('pop');
+} )
